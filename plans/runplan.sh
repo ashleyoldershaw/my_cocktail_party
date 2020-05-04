c6e5e873adb04dec9c8e21fd98e0c8ca66b025e1
ubuntu <iocchi@diag.uniroma1.it>
2020-05-04 11:50:48 +0200

    adding robustness and cleaning up runplan.sh

--- plans/runplan.sh
+++ plans/runplan.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # Use: ./runplan.sh <robotname> <planname>
 # Example: ./runplan.sh diago_0 cocktail_party
+set -e
 
 rosparam set /$1/pnp/plan_folder `pwd`
-./genplan.sh $2.plan $2.er #generatedRules.er
+./genplan.sh $2.plan $2.er
 rostopic pub /$1/planToExec std_msgs/String "data: '$2'" --once
-
